82b5ac6fbdf18f3766594329ae1bc0409f7c4e33
00ZRHun <00zrhun@gmail.com>
2023-02-09 17:03:09 +0800

    add SECRET_KEY & getSigningKey

--- src/main/java/com/alibou/security/config/JwtService.java
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -2,10 +2,15 @@ package com.alibou.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String token) {
         return null;
@@ -18,6 +23,11 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
