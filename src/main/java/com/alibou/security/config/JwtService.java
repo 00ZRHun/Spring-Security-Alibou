8e27bb344ff91e9787ea2b4b50a35af5c23fb642
00ZRHun <00zrhun@gmail.com>
2023-02-09 17:16:32 +0800

    add extractClaim

--- src/main/java/com/alibou/security/config/JwtService.java
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,6 +15,11 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
