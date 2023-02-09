1c3dfb839e79cb52c37316a6244c4af89da33a06
00ZRHun <00zrhun@gmail.com>
2023-02-09 23:41:42 +0800

    implement authenticate

--- src/main/java/com/alibou/security/auth/AuthenticationService.java
+++ src/main/java/com/alibou/security/auth/AuthenticationService.java
@@ -1,13 +1,16 @@
 package com.alibou.security.auth;
 
-import ch.qos.logback.core.encoder.EchoEncoder;
 import com.alibou.security.config.JwtService;
 import com.alibou.security.user.Role;
 import com.alibou.security.user.User;
 import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class AuthenticationService {
     private UserRepository repository;
     private JwtService jwtService;
     private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -33,7 +37,18 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticateRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
 }
