2b4a6db3adaea4cad1af0b30bdb31e5b9ec05daa
00ZRHun <00zrhun@gmail.com>
2023-02-09 23:10:25 +0800

    implement register

--- src/main/java/com/alibou/security/auth/AuthenticationService.java
+++ src/main/java/com/alibou/security/auth/AuthenticationService.java
@@ -1,14 +1,35 @@
 package com.alibou.security.auth;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
+import com.alibou.security.config.JwtService;
+import com.alibou.security.user.Role;
+import com.alibou.security.user.User;
+import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private UserRepository repository;
+    private JwtService jwtService;
+    private PasswordEncoder passwordEncoder;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticateRequest request) {
